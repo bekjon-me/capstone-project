987283ce8e64c9d4997d14837c9b1e75875fac94
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-16 00:13:54 +0500

    Use static data for mentors section

--- src/index.js
+++ src/index.js
@@ -11,3 +11,64 @@ function typeWriter() {
 }
 
 typeWriter();
+
+const cardTitles = document.querySelectorAll('#mentors .card-title');
+const cardTexts = document.querySelectorAll('.text-muted');
+const cardImages = document.querySelectorAll('.img-fluid.rounded-start');
+const descriptions = document.querySelectorAll('.description');
+
+const speakers = [
+  {
+    name: 'Bekjon Ishpulatov',
+    title: 'Software Engineer',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor1.jpg',
+  },
+  {
+    name: 'Steve Vozniak',
+    title: 'Backend Engineer',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor2.jpg',
+  },
+  {
+    name: 'Maria Sharapova',
+    title: 'HR Manager',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor3.jpg',
+  },
+  {
+    name: 'Serena Uilyams',
+    title: 'Frontend Engineer',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor4.webp',
+  },
+  {
+    name: 'Sherlock Holmes',
+    title: 'Fullstack Engineer',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor5.jpeg',
+  },
+  {
+    name: 'John Smith',
+    title: 'Software Engineer',
+    desciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa nulla quam aspernatur voluptatem, cum repellendus sint aliquam sit sequi?',
+    image: './src/images/mentor6.jpeg',
+  },
+];
+
+/// window.load
+window.addEventListener('load', () => {
+  console.log('window loaded');
+  speakers.forEach((speaker, index) => {
+    cardTitles[index].textContent = speaker.name;
+    cardTexts[index].textContent = speaker.title;
+    descriptions[index].textContent = speaker.desciption;
+    cardImages[index].src = speaker.image;
+  });
+});
